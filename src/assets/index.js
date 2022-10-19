cc7b594ea845984aa872accd2ff3a143d3aa637b
Saurabh <singhsaurabh162000@gmail.com>
2022-10-19 14:11:06 +0530

    sidebar logo updated

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,9 @@
 import loader from './loader.svg';
-import logo from './logo.svg';
+import logo from './logo.png';
+import titleLogo from './titleLogo.png';
 
 export {
   logo,
   loader,
+  titleLogo,
 };
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,10 @@
+/* eslint-disable quotes */
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { HiOutlineHashtag, HiOutlineHome, HiOutlineMenu, HiOutlinePhotograph, HiOutlineUserGroup } from 'react-icons/hi';
 import { RiCloseLine } from 'react-icons/ri';
 
-import { logo } from '../assets';
+import { logo, titleLogo } from '../assets';
 
 const links = [
   { name: 'Discover', to: '/', icon: HiOutlineHome },
@@ -33,8 +34,9 @@ const Sidebar = () => {
 
   return (
     <>
-      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
-        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+      <div className="md:flex hidden flex-col w-[240px] py-10 px-4  bg-[#191624]">
+        <img src={logo} alt="logo" className="w-full h-24 object-contain" />
+        <img src={titleLogo} alt="logo" className="w-full h-6 object-contain mt-2" />
         <NavLinks />
       </div>
 
@@ -49,6 +51,7 @@ const Sidebar = () => {
 
       <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483D8B] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
         <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <img src={titleLogo} alt="logo" className="w-full h-6 object-contain mt-1" />
         <NavLinks handleClick={() => setMobileMenuOpen(false)} />
       </div>
     </>
