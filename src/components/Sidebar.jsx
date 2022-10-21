781a09e427ee73dbd84df0d31cacf5805cca95ee
Saurabh <singhsaurabh162000@gmail.com>
2022-10-21 16:17:01 +0530

    fixing bug for responsiveness

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -41,7 +41,7 @@ const Sidebar = () => {
       </div>
 
       {/* Mobile sidebar */}
-      <div className="absolute md:hidden block top-6 right-3">
+      <div className="absolute md:hidden block top-8 right-3">
         {!mobileMenuOpen ? (
           <HiOutlineMenu className="w-6 h-6 mr-2 text-white" onClick={() => setMobileMenuOpen(true)} />
         ) : (
